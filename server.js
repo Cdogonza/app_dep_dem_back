e66077ac59aa9a8854db1becfd154e76c8ecab08
Cdogonza <gonzalo.paz@correo.ucu.edu.uy>
2025-02-19 20:22:12 -0300

    fusion 2dssdeded

--- server.js
+++ server.js
@@ -56,7 +56,6 @@ db.connect(err => {
 app.use('/equipos', require('./routes/equipos'));
 app.use('/novedades', require('./routes/novedades'));
 app.use('/taller', require('./routes/taller'));
-
 app.use('/auth', authRoutes);
 
 
